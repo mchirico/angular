459be376c552c8231623455b399c335b83de903e
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2018-05-03 07:51:15 -0400

    AN-7 Color control
    AN-7 on Heroes
    
    Some more ideas
    
    Better width control

--- src/app/septa/septa.component.ts
+++ src/app/septa/septa.component.ts
@@ -10,6 +10,18 @@ import {HEROES} from '../data/mock-data';
 })
 export class SeptaComponent implements OnInit {
 
+  people: any[] = [
+    {
+      'name': 'Douglas  Pace',
+      'country': 'UK'
+    },
+    {
+      'name': 'Mcleod  Mueller',
+      'country': 'USA'
+    },
+  ];
+
+
   heroes = HEROES;
 
   person: IPerson = {
@@ -31,4 +43,74 @@ export class SeptaComponent implements OnInit {
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+
+  getColor(country) {
+    switch (country) {
+      case 'UK':
+        return 'green';
+      case 'USA':
+        return 'blue';
+      case 'HK':
+        return 'red';
+    }
+  }
+
+  getPaddingPC(country) {
+    switch (country) {
+      case 'UK':
+        return '0.2em 0.2em 0.2em 0.2em';
+      case 'USA':
+        return '0.2em 0.2em 0.2em 0.2em';
+      case 'HK':
+        return '0.2em 0.2em 0.2em 0.2em';
+
+    }
+  }
+
+  getMR(country) {
+    switch (country) {
+      case 'UK':
+        return '15em';
+      case 'USA':
+        return '10em';
+      case 'HK':
+        return '15em';
+
+    }
+  }
+
+  getColor2(name) {
+    switch (name) {
+      case 'Magneta':
+        return 'green';
+      case 'Narco':
+        return 'blue';
+      case 'Bombasto':
+        return 'red';
+    }
+  }
+
+  getBackground(name) {
+    switch (name) {
+      case 'Magneta':
+        return 'blue';
+      case 'Narco':
+        return 'yellow';
+      case 'Bombasto':
+        return 'black';
+    }
+  }
+
+  getPadding(name) {
+    switch (name) {
+      case 'Magneta':
+        return '0.2em 0.2em 0.2em 0.2em';
+      case 'Narco':
+        return '0.8em 0.7em 0 0.7em';
+      case 'Bombasto':
+        return '0.2em 0.2em 0 0.2em';
+    }
+  }
+
+
 }
