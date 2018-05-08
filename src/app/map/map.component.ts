829efca9181d51651a8b1a379834bbbfc3be9383
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2018-05-08 07:58:08 -0400

    AN-23: ng lint

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import {Pipe, PipeTransform, NgModule} from '@angular/core'
-import {BrowserModule, DomSanitizer} from '@angular/platform-browser'
+import {Component, OnInit} from '@angular/core';
+import {Pipe, PipeTransform, NgModule} from '@angular/core';
+import {BrowserModule, DomSanitizer} from '@angular/platform-browser';
 
-@Pipe({ name: 'safe' })
+@Pipe({name: 'safe'})
 export class SafePipe implements PipeTransform {
-  constructor(private sanitizer: DomSanitizer) {}
+  constructor(private sanitizer: DomSanitizer) {
+  }
+
   transform(url) {
     return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
@@ -17,14 +19,15 @@ export class SafePipe implements PipeTransform {
 })
 export class MapComponent implements OnInit {
 
-  constructor(private sanitizer: DomSanitizer) { }
+  constructor(private sanitizer: DomSanitizer) {
+  }
 
   ngOnInit() {
   }
 
-  trustSrcUrl = function(data){
+  trustSrcUrl = function (data) {
     return this.sanitizer.bypassSecurityTrustResourceUrl(data);
-  }
+  };
 
   doSth() {
     console.log('Doing sth...');
