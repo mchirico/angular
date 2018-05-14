e22e23888613ee277bae155292d5ecce8ae7ad97
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2018-05-14 12:19:35 -0400

    AN-28: Initial idea

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-simple-form',
@@ -6,7 +6,7 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./simple-form.component.css']
 })
 export class SimpleFormComponent implements OnInit {
-
+  dataElements = [{type: 'click', name: '', content: ''}];
 
   text0 = '';
   text1 = '';
@@ -18,12 +18,22 @@ export class SimpleFormComponent implements OnInit {
   ngOnInit() {
   }
 
+  onClickTarget(data: { name: string, content: string }) {
+    this.dataElements.push({
+      type: 'click',
+      name: data.name,
+      content: data.content
+    });
+    this.text0 = data.content;
+  }
+
   onAddServer() {
     if (this.text0 === 'del') {
       this.list.pop();
     } else {
       this.list.push(this.text1);
     }
+
   }
 
 }
