c7a5659d9f95914b0c7b66a934cb5bd8df16933d
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2018-05-16 06:25:39 -0400

    AN-29: Authentication, but no tests
    AN-29:  Google login. No test
    
    Clean up

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,14 @@
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCIWxYkXCzyraTMHWfsMdRLhE4bI8bmsz0',
+    authDomain: 'septa-3f6ab.firebaseapp.com',
+    databaseURL: 'https://septa-3f6ab.firebaseio.com',
+    projectId: 'septa-3f6ab',
+    storageBucket: 'septa-3f6ab.appspot.com',
+    messagingSenderId: '577092891343'
+  }
 };
+
+
