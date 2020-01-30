cd75fd24ea113e3b98c00b541123c29da59bb084
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2020-01-30 18:56:13 -0500

    new

--- src/app/server/server.component.spec.ts
+++ src/app/server/server.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import {MapComponent, SafePipe} from './map.component';
+import { ServerComponent } from './server.component';
 
-describe('MapComponent', () => {
-  let component: MapComponent;
-  let fixture: ComponentFixture<MapComponent>;
+describe('ServerComponent', () => {
+  let component: ServerComponent;
+  let fixture: ComponentFixture<ServerComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MapComponent, SafePipe ]
+      declarations: [ ServerComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MapComponent);
+    fixture = TestBed.createComponent(ServerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,16 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: 'AIzaSyCIWxYkXCzyraTMHWfsMdRLhE4bI8bmsz0',
-    authDomain: 'septa-3f6ab.firebaseapp.com',
-    databaseURL: 'https://septa-3f6ab.firebaseio.com',
-    projectId: 'septa-3f6ab',
-    storageBucket: 'septa-3f6ab.appspot.com',
-    messagingSenderId: '577092891343'
-  }
+  production: false
 };
 
-
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
