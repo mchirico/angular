71a87450e2595dca21dedb66682dd7c6945c07c9
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-03-01 10:22:13 -0500

    update

--- src/app/nav/two/detail/detail.component.ts
+++ src/app/nav/two/detail/detail.component.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
+import {SampleService} from '../../../data/sample.service';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
-  styleUrls: ['./detail.component.css']
+  styleUrls: ['./detail.component.css'],
+  providers: [SampleService]
 })
 export class DetailComponent implements OnInit {
   id: number;
-  constructor(private route: ActivatedRoute) { }
+  name: string;
+
+  constructor(private route: ActivatedRoute,
+              private sampleService: SampleService) {
+  }
 
   ngOnInit(): void {
     this.route.params
@@ -16,8 +22,8 @@ export class DetailComponent implements OnInit {
         (params: Params) => {
           this.id = +params.id;
           console.log(this.id);
+          this.name = this.sampleService.getData(this.id).name;
         }
-
       );
   }
 
